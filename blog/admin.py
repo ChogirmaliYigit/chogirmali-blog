8c96c9b8883339c20c0604d0a2459c5b2771637e
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-01-04 14:47:14 +0500

    fix: add truncate content to the blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -10,11 +10,21 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("language", "status", )
 
     def truncated_content(self, obj):
-        # Adjust the length as needed
         max_length = 80
         return (obj.content[:max_length] + '...') if len(obj.content) > max_length else obj.content
 
     truncated_content.short_description = 'Content'
 
 
-admin.site.register(AboutMeSections)
+@admin.register(AboutMeSections)
+class AboutMeSectionsAdmin(admin.ModelAdmin):
+    list_display = ("meta", "title", "truncated_content", "status", "language", )
+    fields = ("meta", "title", "content", "image", "status", "language", )
+    search_fields = ("meta", "title", "content", )
+    list_filter = ("language", "status", )
+
+    def truncated_content(self, obj):
+        max_length = 80
+        return (obj.content[:max_length] + '...') if len(obj.content) > max_length else obj.content
+
+    truncated_content.short_description = 'Content'
