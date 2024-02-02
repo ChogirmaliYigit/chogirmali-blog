846967c307399e234e0c00325038a878151399f5
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-02-02 21:41:13 +0500

    added `STATIC_URL` because of required

--- config/settings.py
+++ config/settings.py
@@ -180,6 +180,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = "static/"
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
